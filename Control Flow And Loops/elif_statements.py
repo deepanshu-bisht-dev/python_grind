5b2eaee3e11be89df49df75f54b0b802e6e5856a
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-05-29 17:18:47 +0530

    Day 2 - Control flow and statements

---
+++ Control Flow And Loops/elif_statements.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age:"))
+
+if(age>18):
+    print("You are eligible to vote")
+elif(age==18):
+    print("Come for an interview")
+elif(age==0):
+    print("Sahi se paida hoja bhai pehle ")
+else:
+    print("You are not eligible to vote")
---
+++ Control Flow And Loops/if_statements.py
@@ -0,0 +1,5 @@
+age = 2
+if(age>18):
+    print("You are eligible to vote")
+
+print("End of program")
