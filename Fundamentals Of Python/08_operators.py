a6c049fb3c0534031a1524b86db1b788b43b1d29
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-05-29 16:37:30 +0530

    Day 2 - Fundamentals of Python

---
+++ Fundamentals Of Python/08_operators.py
@@ -0,0 +1,52 @@
+# Arithmetic Operators
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a%b=",a%b)
+print("a/b=",a/b)
+print("a//b=",a//b)
+print("a**b=",a**b)
+
+#  Comparison Operators : There output is always True or False
+
+print(a>b)
+print(a<b)
+print(a!=b)
+print(a==b)
+print(a>=b)
+print(a>=b)
+
+''' Logical Operators : These are basically three operators OR, AND, NOT
+    There output is also either True or False. As the gates work in 
+    electronics same happen here too.'''
+
+print("OR Operator")
+print(True or False)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("AND Operator")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("NOT Operator")
+print(not True)
+print(not False)
+
+# Assignment Operators : These are used in assigning  values to different variables.
+
+x=12
+x+=4
+print(x)
+
+
+
+
+
+            
+            
