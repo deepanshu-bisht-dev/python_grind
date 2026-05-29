1ed1e0c7778ad553045ab2bc129c3ffc0987a612
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-05-29 22:34:11 +0530

    Day 2 - Control Flow and Loops

---
+++ Control Flow And Loops/match_case.py
@@ -0,0 +1,116 @@
+a = int(input("Enter a number between 1 to 100"))
+
+match a:
+    case 3:
+        print("You won an I Phone")
+    case 53 :
+        print("You won a charger")
+    case 70 :
+        print("You won a tour to Russia")
+    case 93 :
+        print("You won a tour to Manali")
+    case _:
+        print("Better luck next time")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
