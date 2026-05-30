ba88e71afb4ae4f9d1ff5d079adc92b6c05924c7
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-05-30 09:41:26 +0530

    Day 3 - Control Flow and Loops

---
+++ Control Flow And Loops/for_loops.py
@@ -0,0 +1,11 @@
+# For Loops are used to iterate over a sequence.
+for i in range(1,11):
+    print(i)
+
+for i in range(1,11):      
+    print("19 X",i,"=",19*i)
+# range function in this case goes from 1 to (11-1) ie. 10.
+# this is how range function goes.
+
+for i in range(1,19):
+    print("57 X",i,"=",57*i)
