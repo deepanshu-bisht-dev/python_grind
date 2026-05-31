c57f4231fe5ff638cb16357a357e9076deb16926
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-06-01 02:51:14 +0530

    Day 5 - Strings

---
+++ 05-Strings/05_fstrings.py
@@ -0,0 +1,13 @@
+# f-strings are used to format the strings.
+name = "Deepanshu"
+age =30
+print(f"My name is {name} and I am {age} years old.")
+
+# We can directly perform calculation inside the string.
+x = 12
+y = 34
+print(f"The sum of {x} and {y} is {x+y}")
+
+# We can format no. inside the string.
+pi = 3.14159265
+print(f"Pi rounded to two decimal places is {pi:.2f}")
