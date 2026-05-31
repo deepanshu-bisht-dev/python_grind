a25236b94a7e15fa2c4bcec7c0b4954a04cdae87
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-05-31 08:09:11 +0530

    Day 4 - Strings

---
+++ 05-Strings/01strings.py
@@ -0,0 +1,10 @@
+# Types of Strings
+
+# Single Quoted String
+print('Hello World')
+# Double Quoted String
+print("Hello Python")
+# Triple Quoted String
+print(""" This is 
+    a multi line
+    string""")
---
+++ 05-Strings/02indexing.py
@@ -0,0 +1,15 @@
+# Positive Indexing
+name = "Harry"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+# Negative Indexing
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+# To convert negative indexing to positive indeexing add the negative index to the size of string.
