bbdcb4ee6496cf93ae5d18e8eb7afbef99f761b5
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-06-05 09:50:23 +0530

    Day 9 - Data Structures

---
+++ 09-Data Structures In Python/09dictionaries.py
@@ -0,0 +1,8 @@
+# Dictionaries are used too store key value pairs.
+
+marks = {"Alice":67,"Ranveer":45,"Deepanshu":98,"Ahiraj":99,"Harry":87 }
+
+print(marks)
+print(marks["Deepanshu"])
+marks["Mohit"] = 73
+print(marks)
