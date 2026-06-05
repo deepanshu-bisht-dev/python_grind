0b65e5ad8ebe316b04ee384d4bcb26a46d3472e7
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-06-05 23:12:45 +0530

    Day 9 - Practice Set 4

---
+++ 10-Practice Set 04/question11.py
@@ -0,0 +1,13 @@
+'''Create a dictionary of three friends and their phone numbers. Use:
+
+keys() to get all names
+values() to get all numbers
+items() to loop over key-value pairs and print them'''
+
+friends = {"Aryan" : 99277730, "Pooja" : 89546967, "Abhiraj" : 76934590}
+
+print(friends.keys())
+
+print(friends.values())
+
+print(friends.items())
