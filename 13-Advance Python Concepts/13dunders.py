5d624e777e3c1bc22dec26d08f022ae4b9a7036f
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-06-14 10:56:47 +0530

    Day 14 - Advance Concepts Of Python

--- 13-Advance Python Concepts/13dunders.py
+++ 13-Advance Python Concepts/13dunders.py
@@ -1,3 +1,26 @@
 '''__add__ , __sub__ , __mul__ , etc. - Operators Overloading
 These methods allow you to define how your objects behave with standard arithmetic and comparison operators. '''
 
+class Vector:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __add__(self,other):
+        return Vector( self.x + other.x, self.y + other.y )
+    def __sub__(self,other):
+        return Vector( self.x - other.x, self.y - other.y )
+    def __mul__(self, scalar):
+        return Vector( self.x * scalar, self.y * scalar)
+    def __str__(self):
+        return f"Vector({self.x},{self.y})"
+    
+
+v1  = Vector(2,3)
+v2  = Vector(4,5)
+v3 = v1 + v2 # calls __add__
+print(v3)
+v4 = v1 - v2  # calls __sub__
+print(v2)
+v5 = v1 * 5  # calls __mul__
+print(v5)
