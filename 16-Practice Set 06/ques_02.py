edf2711f6dc340268e5d84bc3533fe71b32d7866
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-06-16 16:52:23 +0530

    Day 16 - Practice Set 5

--- 16-Practice Set 06/ques_02.py
+++ 16-Practice Set 06/ques_02.py
@@ -5,9 +5,10 @@ from time import time
 def timer(func):
     def wrapper(n):
         t1 = time()
-        func(n)
+        result = func(n)
         t2 = time()
         print (t2-t1)
+        return result
     return wrapper
 
 @timer
@@ -16,4 +17,7 @@ def sum_1m(n):
     for i in range(1,n+1):
         sum += i
     return sum
+
+a = sum_1m(10000000)
+print(a)
 
---
+++ 16-Practice Set 06/ques_03.py
@@ -0,0 +1,6 @@
+'''Create a class Employee with a private attribute _salary.
+
+Use @property to define a getter for salary.
+Use @salary.setter to prevent setting negative values (print a warning instead).
+Create an object and test by setting positive and negative salaries.'''
+
