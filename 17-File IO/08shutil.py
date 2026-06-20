d9fc241a215c821ca3e4dff689ecb0c0b1781009
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-06-20 23:16:35 +0530

    Day 17 - File IO

---
+++ 17-File IO/08shutil.py
@@ -0,0 +1,11 @@
+# shutil module is same as os module but it is an advanced verssion of it as it can interact with operating system in more advanced manner.
+import shutil
+
+# Copy a file
+# shutil.copy("my_file.txt", "my_file_copy.txt")
+
+# Move a file or directory
+# shutil.move("my_file.txt", "new_directory/")
+
+# For permanently deleting a directory
+# shutil.rmtree("dir")
