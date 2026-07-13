d9f23f6b0b75ac721717b2ca5ce8f8e960114582
Deepanshu Bisht <deepanshu180607@gmail.com>
2026-07-13 20:28:17 +0530

    Change loop range to 1-11 and update comment

--- 04-Practice Set 01/question13.py
+++ 04-Practice Set 01/question13.py
@@ -1,5 +1,5 @@
-# Use a for loop to print numbers from 1 to 10, but stop the loop if the number is 7 (use break).
-for i in range(1,11):
+# Use a for loop to print numbers from 1 to 11, but stop the loop if the number is 7 (use break).
+for i in range(1,12):
     print(i)
     if (i==7):
         break
